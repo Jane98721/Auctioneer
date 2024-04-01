6014fa1bf07e9522e19efef3e0feec1c6947d08c
J L <jl-@live.se>
2024-04-01 21:43:01 +0200

    homepage

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Homepage from './Homepage'
+import './index.css'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    <Homepage/>
+    <Footer/>
+
+    </>
+  )
+}
+
+export default App
+
+
+  
---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+
+function Navbar() {
+
+return (
+
+    <nav>
+        <ul className="navbar">
+            <li className="bar"> <a href="#">Home</a></li>
+            <li className="bar"> <a href="#">Home</a></li>
+        </ul>
+    </nav>
+
+)
+}
+
+export default Navbar
